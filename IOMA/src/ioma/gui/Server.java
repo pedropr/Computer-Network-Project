c24dd20723be927f963500ca0cac27535bfab5c2
Pedro <p.vegabowling@gmail.com>
2019-05-15 19:04:28 -0400

    Fix server and completion

--- IOMA/src/ioma/gui/Server.java
+++ IOMA/src/ioma/gui/Server.java
@@ -21,28 +21,25 @@ public class Server {
             String recieve = new String (packet.getData());
             System.out.println("Message recieve: " + recieve);
             if (recieve.contains("A:")) {
-                System.out.println("New User: " + recieve + " : " + packet.getAddress());
-                String newUser = recieve;
+                System.out.println("New User: " + recieve + " IP: " + packet.getAddress().toString());
                 //Parse goes here
                 String user = recieve.substring(2);
-                System.out.println(user);
+                //System.out.println(user);
 
 
-
-                //Then send user Previos User added
-
-
-                //This will change to for loop
+                //Then send added client to new client
                 for (Users selectuser : usersList) {
-                    String prevUser = "A:" + selectuser.getName();
+                    String prevUser = "A:" + selectuser.getName() + "IP:" + selectuser.getIp();
                     buf = new byte[256];
                     buf = prevUser.getBytes();
-                    System.out.println("Sending previous user: " + prevUser + " User to: " + packet.getAddress());
+                    System.out.println("Sending previous user: " + prevUser + " User to: " + selectuser.getIp());
                     DatagramPacket usernamePacket = new DatagramPacket(buf, buf.length, packet.getAddress(), 50000);
                     socket.send(usernamePacket);
                 }
+                //Add client to list
                 usersList.add(new Users(user, packet.getAddress().toString()));
                 //Then send to broadcast new user added to previous user
+
                 Enumeration interfaces = NetworkInterface.getNetworkInterfaces();
                 while (interfaces.hasMoreElements()) {
                     NetworkInterface networkInterface = (NetworkInterface) interfaces.nextElement();
@@ -56,7 +53,8 @@ public class Server {
                         }
                         // Send the broadcast package!
                         try {
-                            System.out.println("Send the new user added to chat: " + recieve + " to broadcast " + broadcast);
+                            recieve = recieve + "IP:" + packet.getAddress().toString();
+                            System.out.println("Send the new user to other client: " + recieve + " by broadcast: " + broadcast.toString());
                             buf = new byte[256];
                             buf = recieve.getBytes();
                             DatagramPacket sendPacket = new DatagramPacket(buf, buf.length, broadcast, 50000);
@@ -72,37 +70,40 @@ public class Server {
                 if (usersList.contains(new Users(removeUser, packet.getAddress().toString()))) {
                     usersList.remove(new Users(removeUser, packet.getAddress().toString()));
                     System.out.println("User remove: " + removeUser);
+                    Enumeration interfaces = NetworkInterface.getNetworkInterfaces();
+                    while (interfaces.hasMoreElements()) {
+                        NetworkInterface networkInterface = (NetworkInterface) interfaces.nextElement();
+
+                        if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+                            continue; // Don't want to broadcast to the loopback interface
+                        }
+
+                        for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+                            InetAddress broadcast = interfaceAddress.getBroadcast();
+                            if (broadcast == null) {
+                                continue;
+                            }
+
+                            // Send the broadcast package!
+                            try {
+                                buf = new byte[256];
+                                recieve = recieve + "IP:" + packet.getAddress().toString();
+                                System.out.println("Send remove user to client: " + recieve + " to broadcast " + broadcast);
+                                buf = recieve.getBytes();
+                                DatagramPacket sendPacket = new DatagramPacket(buf, buf.length, broadcast, 50000);
+                                socket.send(sendPacket);
+
+                            } catch (Exception e) {
+
+                            }
+                        }
+                    }
                 } else {
                     System.out.println("User is already remove or is not in the list");
                 }
                 //Then send user to remove user to broadcast
 
-                Enumeration interfaces = NetworkInterface.getNetworkInterfaces();
-                while (interfaces.hasMoreElements()) {
-                    NetworkInterface networkInterface = (NetworkInterface) interfaces.nextElement();
 
-                    if (networkInterface.isLoopback() || !networkInterface.isUp()) {
-                        continue; // Don't want to broadcast to the loopback interface
-                    }
-
-                    for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
-                        InetAddress broadcast = interfaceAddress.getBroadcast();
-                        if (broadcast == null) {
-                            continue;
-                        }
-
-                        // Send the broadcast package!
-                        try {
-                            System.out.println("Send remove user to client: " + recieve + " to broadcast " + broadcast);
-                            buf = new byte[256];
-                            buf = recieve.getBytes();
-                            DatagramPacket sendPacket = new DatagramPacket(buf, buf.length, broadcast, 50000);
-                            socket.send(sendPacket);
-
-                        } catch (Exception e) {
-                        }
-                    }
-                }
             }else{
                 System.out.print("Other info: ");
                 System.out.println(recieve);
